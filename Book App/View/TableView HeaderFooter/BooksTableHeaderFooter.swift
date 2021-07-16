4765ef706f62a91dab8181f54fb52041e406f572
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2021-07-16 17:41:38 +0200

    Added the file BooksTableHeaderFooter.swift at directory TableView HeaderFooter in View folder

---
+++ Book App/View/TableView HeaderFooter/BooksTableHeaderFooter.swift
@@ -0,0 +1,22 @@
+//
+//  BooksTableHeaderFooter.swift
+//  Book App
+//
+//  Created by Ndriqim Lahu on 5/1/21.
+//  Copyright © 2021 Ndriqim Lahu. All rights reserved.
+//
+
+import UIKit
+
+class BooksTableHeaderFooter: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
