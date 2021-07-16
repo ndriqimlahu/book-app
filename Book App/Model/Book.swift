3771cf13418f00142513ee31a47c1602542575a1
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2021-07-16 17:39:55 +0200

    Added the file Book.swift in Model folder

---
+++ Book App/Model/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  Book App
+//
+//  Created by Ndriqim Lahu on 3/28/21.
+//  Copyright © 2021 Ndriqim Lahu. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    var picture: String?
+    var name: String?
+    var edition: String?
+    var author: String?
+    var publisher: String?
+    var language: String?
+    var pages: String?
+    var price: String?
+}
